ee224f4246e74ac1d21344926466d7d03cf2c51e
Antonio Guiotto <aguiotto@red.software.systems>
2019-01-30 18:36:07 +0100

    grid content2

--- src/components/Content2/Content2.js
+++ src/components/Content2/Content2.js
@@ -10,19 +10,19 @@ export class Content2 extends Component {
   }
 
   async fetchAPI() {
-    const res = await fetch('https://raw.githubusercontent.com/Antonizero/demo/master/pizze.json');
+    const res = await fetch('https://raw.githubusercontent.com/Antonizero/demo/master/avatars.json');
     const json = await res.json();
     const parentDiv = document.querySelector('.output')
     const div = document.createElement("div");
-    div.setAttribute('id', 'pizze');
+    div.setAttribute('id', 'avatars');
     console.log(div)
-    const jsonData = json.pizze;  
+    const jsonData = json.avatars;  
     console.log(jsonData)
     div.innerHTML = jsonData.map(i => `
-    <div className="card">
-    <h3>${i.name}</h3>
-    <img style="width:100px;" src="./avatar.png">
-    <p>${i.price}</p>
+    <div className="card" style="background-color: black;margin:10px;">
+    <h3 style="margin:0;">${i.name}</h3>
+    <img src="./avatar.png" style="width: 100%;">
+    <p style="margin:0;">${i.superpower}</p>
     </div>
     `).join('');
     parentDiv.appendChild(div)
